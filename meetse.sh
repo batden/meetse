138c9e0d812632dbe1637335b0d39ab27426a484
Philippe Jean Guillaumie <batden@sfr.fr>
2023-09-13 16:32:38 +0200

    Eflete now use meson

--- meetse.sh
+++ meetse.sh
@@ -18,8 +18,7 @@ DISTRO=$(lsb_release -sc)
 DDTL=1.4.1
 
 PROG_MN="efl terminology enlightenment ephoto evisum rage express ecrire enventor edi entice
-enlightenment-module-forecasts"
-PROG_AT="eflete"
+enlightenment-module-forecasts eflete"
 
 beep_exit() {
   aplay --quiet /usr/share/sounds/sound-icons/pipe.wav 2>/dev/null
@@ -96,11 +95,6 @@ uninstall_e26() {
   for I in $PROG_MN; do
     cd $ESRC/e26/$I
     sudo ninja -C build uninstall &>/dev/null
-  done
-
-  for I in $PROG_AT; do
-    cd $ESRC/e26/$I
-    sudo make uninstall &>/dev/null
   done
 
   cd /etc
