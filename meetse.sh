cf48de4e645a59ee6cedda5acbab241c29604e25
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-31 17:08:13 +0100

    Minor correction

--- meetse.sh
+++ meetse.sh
@@ -295,7 +295,7 @@ lo() {
   printf "\n\n$BDR%s $OFF%s\n\n" "All done."
   # Candidates for further deletion: Search for “meetse”, “ebackups” and “pbackups” in your home folder.
   # Note that the binary dependencies (dev packages) are kept for system consistency. There is no safe
-  # way to remove them automatically, without close user supervision.
+  # way to remove them automatically, without close supervision by the user.
 }
 
 lo
